9ba9d6afa88572fdbeee67c5df7723d735bed931
Magath Ale Coumba S NAEL <114144355+mansanael@users.noreply.github.com>
2026-01-23 09:48:43 +0000

    facture avec js

---
+++ factureAutomatique/script.js
@@ -0,0 +1,42 @@
+// On met la date du jour dans le champ "date" du formulaire 
+window.onload = function() {
+    // On  prend la date du jour et on l'affiche dans le champ "date"
+    let aujourdui = new Date();
+    console.log("La date en entier : " + aujourdui);
+    console.log("Le jour : " + aujourdui.getDate());
+    console.log("Le mois : " + (aujourdui.getMonth() +1)); // Les mois commencent à 0
+    let jour = aujourdui.getDate().toString().padStart(2, '0');
+    let mois = (aujourdui.getMonth() + 1).toString().padStart(2, '0'); // Les mois commencent à 0
+    let annee = aujourdui.getFullYear();
+
+    calculncv = function() {
+        let montantHT = document.getElementById("montantht").value || 0;
+        let remisePourcentage = parseFloat(document.getElementById("tauxremise").value) || 0;
+        console.log("Remise : " + remisePourcentage);
+        let montantRemise = montantHT * (remisePourcentage / 100);
+        let montantNCV = montantHT - montantRemise;
+
+        document.getElementById("remise").value = montantRemise.toFixed(2);
+        document.getElementById("ncv").value = montantNCV.toFixed(2);
+    }
+
+    calculncf = function(){
+        let montantNCV = document.getElementById("ncv").value || 0;
+        let transport = document.getElementById("transport").value || 0;
+
+         document.getElementById("ncf").value = (parseFloat(montantNCV) + parseFloat(transport)).toFixed(2);
+  }
+
+    // On recupère le champ date et on lui assigne la date formatée
+    document.getElementById("date").value = `${annee}-${mois}-${jour}`;
+
+    // On met le numéro de facture automatique en utilisant DAAMJ avec AA les deux derniers chiffres de l'année, MM le mois et JJ le jour
+    let numeroFacture = `D${annee.toString().slice(-2)}${mois}${jour}`;
+    document.getElementById("numero").value = numeroFacture;
+
+    document.getElementById("montantht").addEventListener("input",  calculncv );
+
+    document.getElementById("tauxremise").addEventListener("input", calculncv );
+
+    document.getElementById("transport").addEventListener("input", calculncf );
+};
