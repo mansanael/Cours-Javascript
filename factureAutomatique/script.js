aae91d666ebb88fe1822d2e833d1585364e3b40b
Magath Ale Coumba S NAEL <114144355+mansanael@users.noreply.github.com>
2026-01-30 09:51:14 +0000

    calcul du total hors taxe

--- factureAutomatique/script.js
+++ factureAutomatique/script.js
@@ -2,9 +2,6 @@
 window.onload = function() {
     // On  prend la date du jour et on l'affiche dans le champ "date"
     let aujourdui = new Date();
-    console.log("La date en entier : " + aujourdui);
-    console.log("Le jour : " + aujourdui.getDate());
-    console.log("Le mois : " + (aujourdui.getMonth() +1)); // Les mois commencent à 0
     let jour = aujourdui.getDate().toString().padStart(2, '0');
     let mois = (aujourdui.getMonth() + 1).toString().padStart(2, '0'); // Les mois commencent à 0
     let annee = aujourdui.getFullYear();
@@ -12,7 +9,6 @@ window.onload = function() {
     calculncv = function() {
         let montantHT = document.getElementById("montantht").value || 0;
         let remisePourcentage = parseFloat(document.getElementById("tauxremise").value) || 0;
-        console.log("Remise : " + remisePourcentage);
         let montantRemise = montantHT * (remisePourcentage / 100);
         let montantNCV = montantHT - montantRemise;
 
@@ -24,7 +20,17 @@ window.onload = function() {
         let montantNCV = document.getElementById("ncv").value || 0;
         let transport = document.getElementById("transport").value || 0;
 
-         document.getElementById("ncf").value = (parseFloat(montantNCV) + parseFloat(transport)).toFixed(2);
+        document.getElementById("ncf").value = (parseFloat(montantNCV) + parseFloat(transport)).toFixed(2);
+  }
+
+  calculTotalHorsTaxe = function(){
+    let ncf = document.getElementById("ncf").value || 0;
+    let taux = parseFloat(document.getElementById("tauxescompte").value);
+    let escompte = parseFloat(ncf) * taux / 100;
+   
+    document.getElementById("escompte").value = escompte;
+
+    document.getElementById("totalht").value = parseFloat(ncf) - parseFloat(escompte);
   }
 
     // On recupère le champ date et on lui assigne la date formatée
@@ -39,4 +45,6 @@ window.onload = function() {
     document.getElementById("tauxremise").addEventListener("input", calculncv );
 
     document.getElementById("transport").addEventListener("input", calculncf );
+
+    document.getElementById("tauxescompte").addEventListener("input", calculTotalHorsTaxe);
 };
